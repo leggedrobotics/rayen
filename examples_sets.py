c975db9f876179fe93b297018f92716a42b65d27
Jesus Tordesillas <jtorde@mit.edu>
2023-03-09 17:49:16 +0100

    moved examples to another file

---
+++ examples_sets.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+
+def getCube():
+	Aineq=np.array([ [1, 0, 0],
+				 [0, 1, 0],
+				 [0, 0, 1],
+				 [-1, 0, 0],
+				 [0, -1, 0],
+				 [0, 0, -1]]);
+
+	bineq=np.array([[1],
+				[1],
+				[1],
+				[0],
+				[0],
+				[0]])
+
+	return Aineq, bineq
+
+
+def getExample(example):
+
+	if example==0:
+
+		Aineq, bineq=getCube()
+		Aeq=np.array([[1, 1, 1]]);
+		beq=np.array([[1]]);
+
+	elif example==1:
+		Aineq=np.array([[-1,0],
+					 [0, -1],
+					 [0, 1],
+					 [0.2425,    0.9701]]);
+
+		bineq=np.array([[0],
+					[0],
+					[1],
+					[1.2127]])
+
+		Aeq=None;
+		beq=None;
+
+	elif example==2:
+		Aineq, bineq=getCube()
+		Aeq=np.array([[1, 1, 1],
+					  [-1, 1, 1] ]);
+		beq=np.array([[1],[0.1]]);
+
+	elif example==3: #Just a plane
+		Aineq=None
+		bineq=None
+		Aeq=np.array([[1, 1, 1]]);
+		beq=np.array([[1]]);	
+
+	elif example==4: #Unbounded 2d polyhedron. It has two vertices and two rays
+
+		Aineq=np.array([[0,-1], [2,-4], [-2,1]]);
+		bineq=np.array([[-2], [8], [-5]]);
+		Aeq=None
+		beq=None
+
+	else:
+		raise Exception("Not implemented yet")
+
+	return Aineq, bineq, Aeq, beq
