fd302f66c2559e7a6bcbabf7ad9de88ed516f221
Jesus Tordesillas <jtorde@mit.edu>
2023-05-08 17:54:04 +0200

    added more algorithms

--- scripts/run.sh
+++ scripts/run.sh
@@ -16,40 +16,46 @@ tmuxp load train_dataset3d.yaml
 cd ..
 
 #We use the -O flag to remove the asserts, see https://docs.python.org/3/using/cmdline.html#cmdoption-O
-python -O main.py --method walker_1 --dimension_dataset 2  --weight_soft_cost 0    --train False
-python -O main.py --method walker_2 --dimension_dataset 2  --weight_soft_cost 0    --train False
-python -O main.py --method UU       --dimension_dataset 2  --weight_soft_cost 0    --train False
-python -O main.py --method UU       --dimension_dataset 2  --weight_soft_cost 10   --train False
-python -O main.py --method UU       --dimension_dataset 2  --weight_soft_cost 100  --train False
-python -O main.py --method UU       --dimension_dataset 2  --weight_soft_cost 1000 --train False
-python -O main.py --method UP       --dimension_dataset 2  --weight_soft_cost 0    --train False
-python -O main.py --method UP       --dimension_dataset 2  --weight_soft_cost 10   --train False
-python -O main.py --method UP       --dimension_dataset 2  --weight_soft_cost 100  --train False
-python -O main.py --method UP       --dimension_dataset 2  --weight_soft_cost 1000 --train False
-python -O main.py --method DC3      --dimension_dataset 2  --weight_soft_cost 0    --train False
-python -O main.py --method DC3      --dimension_dataset 2  --weight_soft_cost 10   --train False
-python -O main.py --method DC3      --dimension_dataset 2  --weight_soft_cost 100  --train False
-python -O main.py --method DC3      --dimension_dataset 2  --weight_soft_cost 1000 --train False
-python -O main.py --method PP       --dimension_dataset 2  --weight_soft_cost 0    --train False
-python -O main.py --method Bar      --dimension_dataset 2  --weight_soft_cost 0    --train False
+python -O main.py --method walker_1 --dimension_dataset 2  --weight_soft_cost 0     --train False
+python -O main.py --method walker_2 --dimension_dataset 2  --weight_soft_cost 0     --train False
+python -O main.py --method UU       --dimension_dataset 2  --weight_soft_cost 0     --train False
+python -O main.py --method UU       --dimension_dataset 2  --weight_soft_cost 10    --train False
+python -O main.py --method UU       --dimension_dataset 2  --weight_soft_cost 100   --train False
+python -O main.py --method UU       --dimension_dataset 2  --weight_soft_cost 1000  --train False
+python -O main.py --method UU       --dimension_dataset 2  --weight_soft_cost 10000 --train False
+python -O main.py --method UP       --dimension_dataset 2  --weight_soft_cost 0     --train False
+python -O main.py --method UP       --dimension_dataset 2  --weight_soft_cost 10    --train False
+python -O main.py --method UP       --dimension_dataset 2  --weight_soft_cost 100   --train False
+python -O main.py --method UP       --dimension_dataset 2  --weight_soft_cost 1000  --train False
+python -O main.py --method UP       --dimension_dataset 2  --weight_soft_cost 10000 --train False
+python -O main.py --method DC3      --dimension_dataset 2  --weight_soft_cost 0     --train False
+python -O main.py --method DC3      --dimension_dataset 2  --weight_soft_cost 10    --train False
+python -O main.py --method DC3      --dimension_dataset 2  --weight_soft_cost 100   --train False
+python -O main.py --method DC3      --dimension_dataset 2  --weight_soft_cost 1000  --train False
+python -O main.py --method DC3      --dimension_dataset 2  --weight_soft_cost 10000 --train False
+python -O main.py --method PP       --dimension_dataset 2  --weight_soft_cost 0     --train False
+python -O main.py --method Bar      --dimension_dataset 2  --weight_soft_cost 0     --train False
 
 
 #Note that the Bar method cannot be used with quadratic constraints
-python -O main.py --method walker_1 --dimension_dataset 3  --weight_soft_cost 0    --train False
-python -O main.py --method walker_2 --dimension_dataset 3  --weight_soft_cost 0    --train False
-python -O main.py --method UU       --dimension_dataset 3  --weight_soft_cost 0    --train False
-python -O main.py --method UU       --dimension_dataset 3  --weight_soft_cost 10   --train False
-python -O main.py --method UU       --dimension_dataset 3  --weight_soft_cost 100  --train False
-python -O main.py --method UU       --dimension_dataset 3  --weight_soft_cost 1000 --train False
-python -O main.py --method UP       --dimension_dataset 3  --weight_soft_cost 0    --train False
-python -O main.py --method UP       --dimension_dataset 3  --weight_soft_cost 10   --train False
-python -O main.py --method UP       --dimension_dataset 3  --weight_soft_cost 100  --train False
-python -O main.py --method UP       --dimension_dataset 3  --weight_soft_cost 1000 --train False
-python -O main.py --method DC3      --dimension_dataset 3  --weight_soft_cost 0    --train False
-python -O main.py --method DC3      --dimension_dataset 3  --weight_soft_cost 10   --train False
-python -O main.py --method DC3      --dimension_dataset 3  --weight_soft_cost 100  --train False
-python -O main.py --method DC3      --dimension_dataset 3  --weight_soft_cost 1000 --train False
-python -O main.py --method PP       --dimension_dataset 3  --weight_soft_cost 0 --train False
+python -O main.py --method walker_1 --dimension_dataset 3  --weight_soft_cost 0     --train False
+python -O main.py --method walker_2 --dimension_dataset 3  --weight_soft_cost 0     --train False
+python -O main.py --method UU       --dimension_dataset 3  --weight_soft_cost 0     --train False
+python -O main.py --method UU       --dimension_dataset 3  --weight_soft_cost 10    --train False
+python -O main.py --method UU       --dimension_dataset 3  --weight_soft_cost 100   --train False
+python -O main.py --method UU       --dimension_dataset 3  --weight_soft_cost 1000  --train False
+python -O main.py --method UU       --dimension_dataset 3  --weight_soft_cost 10000 --train False
+python -O main.py --method UP       --dimension_dataset 3  --weight_soft_cost 0     --train False
+python -O main.py --method UP       --dimension_dataset 3  --weight_soft_cost 10    --train False
+python -O main.py --method UP       --dimension_dataset 3  --weight_soft_cost 100   --train False
+python -O main.py --method UP       --dimension_dataset 3  --weight_soft_cost 1000  --train False
+python -O main.py --method UP       --dimension_dataset 3  --weight_soft_cost 10000 --train False
+python -O main.py --method DC3      --dimension_dataset 3  --weight_soft_cost 0     --train False
+python -O main.py --method DC3      --dimension_dataset 3  --weight_soft_cost 10    --train False
+python -O main.py --method DC3      --dimension_dataset 3  --weight_soft_cost 100   --train False
+python -O main.py --method DC3      --dimension_dataset 3  --weight_soft_cost 1000  --train False
+python -O main.py --method DC3      --dimension_dataset 3  --weight_soft_cost 10000 --train False
+python -O main.py --method PP       --dimension_dataset 3  --weight_soft_cost 0     --train False
 
 
 cd $SCRIPT_DIR
