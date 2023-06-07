19021927696687ea7d770a3c2779bf029a4c8961
Jesus Tordesillas <jtorde@mit.edu>
2023-06-07 14:55:50 +0200

    minor

--- examples/other/test_root_find.py
+++ examples/other/test_root_find.py
@@ -1,48 +1,44 @@
 
 
-from root import root_find
-import torch
-import cvxpy as cp
+# from root import root_find
+# import torch
+# import cvxpy as cp
 
-# initial guess
-
-
->>> x.value = 3
->>> Si[0].value
-
-x_cp = cp.Variable(2,1)
-expression=x_cp[0,0]*x_cp[0,0] - x_cp[1,0]
+# # initial guess
 
 
-# function to optimize
-def my_func(x):
-    x_cp.value=x;
-    expression=
-    return x[0,0]*x[0,0] - x[1,0] #Set is x^2-y<=0
-    # return x - torch.cos(x)
+# x_cp = cp.Variable(2,1)
+# expression=x_cp[0,0]*x_cp[0,0] - x_cp[1,0]
 
-def my_tracer(alpha):
-    v_n=torch.Tensor([[0.5],[1]])
-    z0=torch.Tensor([[0],[0.1]])
-    return my_func(z0+alpha*alpha*v_n)
 
-def newton(func, guess, runs=5000): 
-    for _ in range(runs): 
-        # evaluate our function with current value of `guess`
-        value = func(guess)
-        value.backward()
-        # update our `guess` based on the gradient
-        guess.data -= (value / guess.grad).data
-        # zero out current gradient to hold new gradients in next iteration 
-        guess.grad.data.zero_() 
-        print(guess)
-    return guess.data # return our final `guess` after 5 updates
+# # function to optimize
+# def my_func(x):
+#     x_cp.value=x;
+#     return x[0,0]*x[0,0] - x[1,0] #Set is x^2-y<=0
+#     # return x - torch.cos(x)
 
-# call starts
-guess = torch.Tensor([1], dtype=torch.float64, requires_grad = True) 
-result = newton(my_tracer, guess)
+# def my_tracer(alpha):
+#     v_n=torch.Tensor([[0.5],[1]])
+#     z0=torch.Tensor([[0],[0.1]])
+#     return my_func(z0+alpha*alpha*v_n)
 
-print(my_tracer(result))
+# def newton(func, guess, runs=5000): 
+#     for _ in range(runs): 
+#         # evaluate our function with current value of `guess`
+#         value = func(guess)
+#         value.backward()
+#         # update our `guess` based on the gradient
+#         guess.data -= (value / guess.grad).data
+#         # zero out current gradient to hold new gradients in next iteration 
+#         guess.grad.data.zero_() 
+#         print(guess)
+#     return guess.data # return our final `guess` after 5 updates
+
+# # call starts
+# guess = torch.Tensor([1], dtype=torch.float64, requires_grad = True) 
+# result = newton(my_tracer, guess)
+
+# print(my_tracer(result))
 # output of `result`
 # tensor([0.7391], dtype=torch.float64)
 
